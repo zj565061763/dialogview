fb4fa0bd32d08882b87e7768d633e597b3220be0
zjun <565061763@qq.com>
2018-06-06 17:14:04 +0800

    demo

--- app/src/main/java/com/fanwe/dialogview/ConfirmDialoger.java
+++ app/src/main/java/com/fanwe/dialogview/ConfirmDialoger.java
@@ -1,26 +1,24 @@
 package com.fanwe.dialogview;
 
 import android.app.Activity;
-import android.view.View;
 
 import com.fanwe.lib.dialoger.animator.ScaleXYCreater;
 import com.fanwe.lib.dialoger.impl.FDialoger;
-import com.fanwe.lib.dialogview.ConfirmView;
 import com.fanwe.lib.dialogview.impl.FConfirmView;
 
 public class ConfirmDialoger extends FDialoger
 {
-    private ConfirmView mConfirmView;
+    private FConfirmView mConfirmView;
 
     public ConfirmDialoger(Activity activity)
     {
         super(activity);
-        setContentView((View) getConfirmView());
+        setContentView(getConfirmView());
         setPadding(0, 0, 0, 0);
         setAnimatorCreater(new ScaleXYCreater());
     }
 
-    public ConfirmView getConfirmView()
+    public FConfirmView getConfirmView()
     {
         if (mConfirmView == null)
             mConfirmView = new FConfirmView(getContext());
