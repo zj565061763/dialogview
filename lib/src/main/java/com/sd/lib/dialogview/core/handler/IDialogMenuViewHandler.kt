9febd7226a3b663c22420e9474557659f7e82c56
zjun <565061763@qq.com>
2021-05-13 21:32:30 +0800

    kotlin IDialogMenuViewHandler

--- lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogMenuViewHandler.kt
+++ lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogMenuViewHandler.kt
@@ -1,7 +1,5 @@
-package com.sd.lib.dialogview.core.handler;
+package com.sd.lib.dialogview.core.handler
 
-import com.sd.lib.dialogview.DialogMenuView;
+import com.sd.lib.dialogview.DialogMenuView
 
-public interface IDialogMenuViewHandler extends IDialogViewHandler<DialogMenuView>
-{
-}
+interface IDialogMenuViewHandler : IDialogViewHandler<DialogMenuView>
