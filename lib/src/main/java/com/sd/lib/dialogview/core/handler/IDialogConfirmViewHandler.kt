ddc95ae6fea74e4f83a728b75260e64935e123c9
zjun <565061763@qq.com>
2021-05-13 21:32:57 +0800

    kotlin IDialogConfirmViewHandler

--- lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogConfirmViewHandler.kt
+++ lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogConfirmViewHandler.kt
@@ -1,7 +1,5 @@
-package com.sd.lib.dialogview.core.handler;
+package com.sd.lib.dialogview.core.handler
 
-import com.sd.lib.dialogview.DialogConfirmView;
+import com.sd.lib.dialogview.DialogConfirmView
 
-public interface IDialogConfirmViewHandler extends IDialogViewHandler<DialogConfirmView>
-{
-}
+interface IDialogConfirmViewHandler : IDialogViewHandler<DialogConfirmView>
