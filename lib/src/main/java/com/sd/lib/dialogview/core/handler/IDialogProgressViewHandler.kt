62310d984bcd74db58294305164bf891f7e0ef52
zjun <565061763@qq.com>
2021-05-13 21:31:50 +0800

    kotlin IDialogProgressViewHandler

--- lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogProgressViewHandler.kt
+++ lib/src/main/java/com/sd/lib/dialogview/core/handler/IDialogProgressViewHandler.kt
@@ -1,8 +1,7 @@
-package com.sd.lib.dialogview.core.handler;
+package com.sd.lib.dialogview.core.handler
 
-import com.sd.lib.dialogview.DialogProgressView;
+import com.sd.lib.dialogview.DialogProgressView
 
-public interface IDialogProgressViewHandler extends IDialogViewHandler<DialogProgressView>
-{
-    void setTextMsg(DialogProgressView dialogView, String text);
+interface IDialogProgressViewHandler : IDialogViewHandler<DialogProgressView> {
+    fun setTextMsg(dialogView: DialogProgressView, text: String?)
 }
