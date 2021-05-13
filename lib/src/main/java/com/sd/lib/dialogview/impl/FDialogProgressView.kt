a5a6b4d7a5dca9fd2b331e8be7c523edfeed1e3c
zjun <565061763@qq.com>
2021-05-13 22:42:26 +0800

    kotlin FDialogProgressView

--- lib/src/main/java/com/sd/lib/dialogview/impl/FDialogProgressView.kt
+++ lib/src/main/java/com/sd/lib/dialogview/impl/FDialogProgressView.kt
@@ -1,127 +1,98 @@
-package com.sd.lib.dialogview.impl;
+package com.sd.lib.dialogview.impl
 
-import android.content.Context;
-import android.text.TextUtils;
-import android.util.AttributeSet;
-import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.sd.lib.dialoger.Dialoger;
-import com.sd.lib.dialogview.DialogProgressView;
-import com.sd.lib.dialogview.R;
-import com.sd.lib.dialogview.core.DialogViewManager;
-import com.sd.lib.dialogview.core.handler.IDialogProgressViewHandler;
+import android.content.Context
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.sd.lib.dialoger.Dialoger
+import com.sd.lib.dialogview.DialogProgressView
+import com.sd.lib.dialogview.R
+import com.sd.lib.dialogview.core.DialogViewManager
+import com.sd.lib.dialogview.core.handler.IDialogProgressViewHandler
 
 /**
  * 带环形进度条，和信息提示
  */
-public class FDialogProgressView extends BaseDialogView implements DialogProgressView
-{
-    public TextView tv_msg;
-    public ProgressBar pb_progress;
-    private boolean mConsumeTouchEvent;
+class FDialogProgressView : BaseDialogView, DialogProgressView {
+    var tv_msg: TextView? = null
+    var pb_progress: ProgressBar? = null
 
-    private final IDialogProgressViewHandler mHandler;
+    private var _consumeTouchEvent = false
+    private val _handler: IDialogProgressViewHandler?
 
-    public FDialogProgressView(Context context)
-    {
-        this(context, null);
-    }
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
+        var layoutId = R.layout.lib_dialogview_progress_view
 
-    public FDialogProgressView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-        mHandler = DialogViewManager.INSTANCE.getDialogViewHandlerFactory().newProgressViewHandler(this);
-
-        int layoutId = R.layout.lib_dialogview_progress_view;
-        if (mHandler != null)
-        {
-            final int id = mHandler.getContentViewResId(this);
-            if (id != 0)
-                layoutId = id;
+        _handler = DialogViewManager.dialogViewHandlerFactory.newProgressViewHandler(this)
+        _handler?.let {
+            val id = it.getContentViewResId(this)
+            if (id != 0) {
+                layoutId = id
+            }
         }
 
-        setContentView(layoutId);
+        setContentView(layoutId)
 
-        if (getLayoutParams() == null)
-        {
-            setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT));
+        val params = layoutParams
+        if (params == null) {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
         }
     }
 
-    @Override
-    protected void onContentViewChanged()
-    {
-        super.onContentViewChanged();
-        tv_msg = findViewById(R.id.tv_msg);
-        pb_progress = findViewById(R.id.pb_progress);
-
-        if (mHandler != null)
-            mHandler.onContentViewChanged(this);
+    override fun onContentViewChanged() {
+        super.onContentViewChanged()
+        tv_msg = findViewById(R.id.tv_msg)
+        pb_progress = findViewById(R.id.pb_progress)
+        _handler?.onContentViewChanged(this)
     }
 
-    @Override
-    protected void initDialog(Dialoger dialog)
-    {
-        super.initDialog(dialog);
-        dialog.setPadding(0, 0, 0, 0);
-        dialog.setGravity(Gravity.CENTER);
+    override fun initDialog(dialog: Dialoger) {
+        super.initDialog(dialog)
+        dialog.setPadding(0, 0, 0, 0)
+        dialog.gravity = Gravity.CENTER
     }
 
-    @Override
-    public DialogProgressView setConsumeTouchEvent(boolean consume)
-    {
-        mConsumeTouchEvent = consume;
-        return this;
+    override fun setConsumeTouchEvent(consume: Boolean): DialogProgressView {
+        _consumeTouchEvent = consume
+        return this
     }
 
-    @Override
-    public DialogProgressView setTextMsg(String text)
-    {
-        if (TextUtils.isEmpty(text))
-        {
-            tv_msg.setVisibility(View.GONE);
-        } else
-        {
-            tv_msg.setVisibility(View.VISIBLE);
-            tv_msg.setText(text);
+    override fun setTextMsg(text: String?): DialogProgressView {
+        tv_msg?.let {
+            if (TextUtils.isEmpty(text)) {
+                it.visibility = GONE
+            } else {
+                it.text = text
+                it.visibility = VISIBLE
+            }
         }
-
-        if (mHandler != null)
-            mHandler.setTextMsg(this, text);
-
-        return this;
+        _handler?.setTextMsg(this, text)
+        return this
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        if (mConsumeTouchEvent)
-        {
-            super.onTouchEvent(event);
-            return true;
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (_consumeTouchEvent) {
+            super.onTouchEvent(event)
+            return true
         }
-        return super.onTouchEvent(event);
+        return super.onTouchEvent(event)
     }
 
-    @Override
-    protected void onAttachedToWindow()
-    {
-        super.onAttachedToWindow();
-        if (mHandler != null)
-            mHandler.onAttachedToWindow(this);
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        _handler?.onAttachedToWindow(this)
     }
 
-    @Override
-    protected void onDetachedFromWindow()
-    {
-        super.onDetachedFromWindow();
-        if (mHandler != null)
-            mHandler.onDetachedFromWindow(this);
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        _handler?.onDetachedFromWindow(this)
     }
 }
