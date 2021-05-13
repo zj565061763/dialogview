574b307a2d3b459ebd88e73a71c4beb4c66012a9
zjun <565061763@qq.com>
2021-05-13 22:42:12 +0800

    DialogViewManager

--- lib/src/main/java/com/sd/lib/dialogview/core/DialogViewManager.kt
+++ lib/src/main/java/com/sd/lib/dialogview/core/DialogViewManager.kt
@@ -8,29 +8,17 @@ import com.sd.lib.dialogview.core.handler.IDialogMenuViewHandler
 import com.sd.lib.dialogview.core.handler.IDialogProgressViewHandler
 
 object DialogViewManager {
-
-    var dialogViewHandlerFactory: IDialogViewHandlerFactory? = null
-        get() {
-            return if (field == null) {
-                _dialogViewHandlerFactoryDefault
-            } else {
-                field
-            }
+    var dialogViewHandlerFactory: IDialogViewHandlerFactory = object : IDialogViewHandlerFactory {
+        override fun newProgressViewHandler(dialogView: DialogProgressView): IDialogProgressViewHandler? {
+            return null
         }
 
-    private val _dialogViewHandlerFactoryDefault: IDialogViewHandlerFactory by lazy {
-        object : IDialogViewHandlerFactory {
-            override fun newProgressViewHandler(dialogView: DialogProgressView): IDialogProgressViewHandler? {
-                return null
-            }
+        override fun newConfirmViewHandler(dialogView: DialogConfirmView): IDialogConfirmViewHandler? {
+            return null
+        }
 
-            override fun newConfirmViewHandler(dialogView: DialogConfirmView): IDialogConfirmViewHandler? {
-                return null
-            }
-
-            override fun newMenuViewHandler(dialogView: DialogMenuView): IDialogMenuViewHandler? {
-                return null
-            }
+        override fun newMenuViewHandler(dialogView: DialogMenuView): IDialogMenuViewHandler? {
+            return null
         }
     }
 }
