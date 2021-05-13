15ce0e7aea81a1c824bcecf3c22d37aa28840e6e
zjun <565061763@qq.com>
2021-05-13 22:26:58 +0800

    kotlin BaseDialogView

--- lib/src/main/java/com/sd/lib/dialogview/impl/BaseDialogView.kt
+++ lib/src/main/java/com/sd/lib/dialogview/impl/BaseDialogView.kt
@@ -1,90 +1,70 @@
-package com.sd.lib.dialogview.impl;
+package com.sd.lib.dialogview.impl
 
-import android.app.Activity;
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.FrameLayout;
+import android.app.Activity
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.sd.lib.dialoger.Dialoger
+import com.sd.lib.dialoger.impl.FDialoger
+import com.sd.lib.dialogview.DialogView
 
-import com.sd.lib.dialoger.Dialoger;
-import com.sd.lib.dialoger.impl.FDialoger;
-import com.sd.lib.dialogview.DialogView;
-
-public class BaseDialogView extends FrameLayout implements DialogView, View.OnClickListener
-{
-    private Dialoger mDialoger;
-
-    public BaseDialogView(Context context)
-    {
-        this(context, null);
-    }
-
-    public BaseDialogView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-    }
-
-    public void setContentView(int layoutId)
-    {
-        removeAllViews();
-        LayoutInflater.from(getContext()).inflate(layoutId, this, true);
-        onContentViewChanged();
-    }
-
-    public void setContentView(View view)
-    {
-        removeAllViews();
-        addView(view);
-        onContentViewChanged();
-    }
-
-    protected void onContentViewChanged()
-    {
-
-    }
-
-    @Override
-    public void onClick(View v)
-    {
-
-    }
-
-    @Override
-    public Dialoger getDialoger()
-    {
-        if (mDialoger == null)
-        {
-            mDialoger = new FDialoger((Activity) getContext());
-            mDialoger.setContentView(this);
-            initDialog(mDialoger);
+open class BaseDialogView : FrameLayout, DialogView, View.OnClickListener {
+    private val _dialogLazy = lazy {
+        FDialoger(context as Activity).apply {
+            this.contentView = this@BaseDialogView
+            initDialog(this)
         }
-        return mDialoger;
     }
 
-    protected void initDialog(Dialoger dialog)
-    {
-        dialog.setCanceledOnTouchOutside(false);
+    override val dialoger: Dialoger by _dialogLazy
+
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs)
+
+    /**
+     * 设置内容View布局id
+     */
+    fun setContentView(layoutId: Int) {
+        removeAllViews()
+        LayoutInflater.from(context).inflate(layoutId, this, true)
+        onContentViewChanged()
     }
 
-    @Override
-    public void dismiss()
-    {
-        if (mDialoger != null)
-        {
-            mDialoger.dismiss();
-        } else
-        {
-            final ViewParent parent = getParent();
-            if (parent instanceof ViewGroup)
-            {
-                try
-                {
-                    ((ViewGroup) parent).removeView(this);
-                } catch (Exception e)
-                {
+    /**
+     * 设置内容View
+     */
+    fun setContentView(view: View?) {
+        removeAllViews()
+        addView(view)
+        onContentViewChanged()
+    }
+
+    override fun onClick(v: View?) {}
+
+    /**
+     * 内容View变化
+     */
+    protected open fun onContentViewChanged() {}
+
+    /**
+     * 初始化[Dialoger]对象
+     */
+    protected open fun initDialog(dialog: Dialoger) {
+        dialog.setCanceledOnTouchOutside(false)
+    }
+
+    override fun dismiss() {
+        if (_dialogLazy.isInitialized()) {
+            dialoger.dismiss()
+        } else {
+            val parent = parent
+            if (parent is ViewGroup) {
+                try {
+                    parent.removeView(this)
+                } catch (e: Exception) {
                 }
             }
         }
