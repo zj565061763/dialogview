60f15ce55c0cc3686a85ac6e07f58edce0b41993
zjun <565061763@qq.com>
2021-06-05 14:56:32 +0800

    Deprecated dialoger

--- lib/src/main/java/com/sd/lib/dialogview/DialogView.kt
+++ lib/src/main/java/com/sd/lib/dialogview/DialogView.kt
@@ -4,6 +4,7 @@ import com.sd.lib.dialog.IDialog
 import com.sd.lib.dialoger.Dialoger
 
 interface DialogView {
+    @Deprecated("use dialogv")
     val dialoger: Dialoger
 
     val dialogv: IDialog
